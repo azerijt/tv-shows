0b26967963daea89a8a963442312757418e7b6f0
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-03-21 15:38:27 +0000

    fixed tests

--- src/utils/clean.test.ts
+++ src/utils/clean.test.ts
@@ -3,5 +3,5 @@ import { clean } from "./clean";
 test("clean removes all <p> and </p> tags from summary text", () => {
   expect(clean("World<p>")).toBe("World");
   expect(clean("Ric</p>hard")).toBe("Richard");
-  expect(clean("Academy </p> Scholars")).toBe("Academy Scholars");
+  expect(clean("Academy</p> Scholars")).toBe("Academy Scholars");
 });
