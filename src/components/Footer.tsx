d2f93a4b225881ee09871f79ebcdcd50a7636a1d
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-03-21 14:06:23 +0000

    Added footer with link to TVMaze

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer(): JSX.Element{
+    return(
+        <div>
+        All data obtained from 
+        <a href="https://www.tvmaze.com/"> TVMaze.com</a>
+        </div>
+    )
+}
