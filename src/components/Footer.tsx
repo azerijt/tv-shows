ce7d542c2df2c10ab34886e9035c6d1796e25aff
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-03-23 17:13:32 +0000

    css styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer(): JSX.Element {
   return (
-    <div>
+    <div className="Container">
       All data obtained from
       <a href="https://www.tvmaze.com/"> TVMaze.com</a>
     </div>
--- src/utils/clean.ts
+++ src/utils/clean.ts
@@ -1,3 +1,7 @@
 export function clean(summary: string): string {
-  return summary.replaceAll("<p>", "").replaceAll("</p>", "");
+  return summary
+    .replaceAll("<p>", "")
+    .replaceAll("</p>", "")
+    .replaceAll("<b>", "")
+    .replaceAll("</b>", "");
 }
